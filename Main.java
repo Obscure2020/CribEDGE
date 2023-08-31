5db70accb12048a29222cdc0d34c3e0d73a746dc
Joey Sodergren <jas.elgoog@gmail.com>
2023-08-31 00:10:43 -0400

    Introducing score consideration.

--- Main.java
+++ Main.java
@@ -11,18 +11,44 @@ public class Main {
         throw new IllegalArgumentException("Bad response. \"" + userInput + "\" is not reasonably interpreted as a \"y\" or an \"n\".");
     }
 
-    private static HandPick pickHand(ArrayList<PlayingCard> cards, boolean dealer){
+    private static HandPick pickHand(ArrayList<PlayingCard> cards, boolean dealer, int myPoints, int theirPoints){
         ArrayList<HandPick> choices = new ArrayList<>();
         for(int i=0; i<6; i++){
             for(int j=i+1; j<6; j++){
                 choices.add(new HandPick(cards, i, j, dealer));
             }
         }
+        Collections.sort(choices, Comparator.comparingInt(HandPick::getWorth).reversed());
+        int maxWorth = choices.get(0).getWorth();
+        ArrayList<HandPick> worthiest = new ArrayList<>();
+        for(HandPick hp : choices){
+            if(hp.getWorth() < maxWorth) break;
+            worthiest.add(hp);
+        }
+        Collections.sort(worthiest, Comparator.comparingInt(HandPick::getBonus).reversed());
+        boolean victory = worthiest.get(0).getWorth() + myPoints > 120;
+        boolean worried = theirPoints >= 80;
+        if(victory || worried){
+            if(victory){
+                System.out.println("Victory lap activated. Maximizing hand worth.");
+            } else {
+                System.out.println("Worried about opponent score. Prioritizing hand worth.");
+            }
+            if(debugInfo){
+                System.out.println("==== DEBUG ====");
+                for(HandPick hp : worthiest){
+                    System.out.println(hp.fancyString());
+                    System.out.println();
+                }
+                System.out.println("==== DEBUG ====");
+            }
+            return worthiest.get(0);
+        }
         Collections.sort(choices);
         if(debugInfo){
             System.out.println("==== DEBUG ====");
-            for(HandPick h : choices){
-                System.out.println(h.fancyString());
+            for(HandPick hp : choices){
+                System.out.println(hp.fancyString());
                 System.out.println();
             }
             System.out.println("==== DEBUG ====");
@@ -33,8 +59,12 @@ public class Main {
     public static void main(String[] args) throws Exception{
         CribbageDeck deck = new CribbageDeck();
         Scanner scan = new Scanner(System.in);
-        int points = 0;
         ArrayList<PlayingCard> hand = new ArrayList<>(6);
+
+        System.out.print("     Our score: ");
+        int myPoints = Integer.parseInt(scan.nextLine());
+        System.out.print("Opponent score: ");
+        int theirPoints = Integer.parseInt(scan.nextLine());
 
         System.out.print("Are we beginning as the Dealer? (y/n) - ");
         boolean dealer = yes_or_no(scan.nextLine());
@@ -50,7 +80,7 @@ public class Main {
         for(PlayingCard k : hand) System.out.println(k.getLongName());
         System.out.println();
 
-        HandPick choice = pickHand(hand, dealer);
+        HandPick choice = pickHand(hand, dealer, myPoints, theirPoints);
         System.out.println(choice.fancyString());
         System.out.println();
 
